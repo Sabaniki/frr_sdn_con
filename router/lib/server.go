ddf46569046eda0cf2a2d7ab71ce964c8acdefc6
Sabaniki <vsix@vsix.wide.ad.jp>
2022-01-01 16:17:40 +0000

    bgp の状態持ってこられるようになったよ

--- router/lib/server.go
+++ router/lib/server.go
@@ -2,105 +2,29 @@ package router
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	pb "github.com/Sabaniki/frr_sdn_con/pb/api"
+	"github.com/mattn/go-pipeline"
 )
 
 type Router struct{}
 
-type BgpIpv6Summary struct {
-	RouterId        string              `json:"routerId,omitempty"`
-	As              int32               `json:"as,omitempty"`
-	VrfId           int32               `json:"vrfId,omitempty"`
-	VrfName         string              `json:"vrfName,omitempty"`
-	TableVersion    int32               `json:"tableVersion,omitempty"`
-	RibCount        int32               `json:"ribCount,omitempty"`
-	RibMemory       int32               `json:"ribMemory,omitempty"`
-	PeerCount       int32               `json:"peerCount,omitempty"`
-	PeerMemory      int32               `json:"peerMemory,omitempty"`
-	PeerGroupCount  int32               `json:"peerGroupCount,omitempty"`
-	PeerGroupMemory int32               `json:"peerGroupMemory,omitempty"`
-	Peers           map[string]PeerInfo `json:"peers,omitempty`
-}
-
-type PeerInfo struct {
-	RemoteAs                   int32  `json:"remoteAs,omitempty"`
-	LocalAs                    int32  `json:"localAs,omitempty"`
-	Version                    int32  `json:"version,omitempty"`
-	MsgRcvd                    int32  `json:"msgRcvd,omitempty"`
-	MsgSent                    int32  `json:"msgSent,omitempty"`
-	TableVersion               int32  `json:"tableVersion,omitempty"`
-	Outq                       int32  `json:"outq,omitempty"`
-	Inq                        int32  `json:"inq,omitempty"`
-	PeerUptime                 string `json:"peerUptime,omitempty"`
-	PeerUptimeMsec             uint32 `json:"peerUptimeMsec,omitempty"` // TODO: proto の方も int32 → uint32 (or int64) にしないとアカン
-	PeerUptimeEstablishedEpoch uint32 `json:"peerUptimeEstablishedEpoch,omitempty"`
-	PfxRcd                     int32  `json:"pfxRcd,omitempty"`
-	PfxSnt                     int32  `json:"pfxSnt,omitempty"`
-	State                      string `json:"state,omitempty"`
-	PeerState                  string `json:"peerState,omitempty"`
-	ConnectionsEstablished     int32  `json:"connectionsEstablished,omitempty"`
-	ConnectionsDropped         int32  `json:"connectionsDropped,omitempty"`
-	Desc                       string `json:"desc,omitempty"`
-	IdType                     string `json:"idType,omitempty"`
-}
-
 func (r *Router) ShowBgpIpv6Summary(ctx context.Context, req *pb.ShowBgpIpv6SummaryRequest) (*pb.ShowBgpIpv6SummaryResult, error) {
-	return &pb.ShowBgpIpv6SummaryResult{
-		RouterId:        "1.1.1.1",
-		As:              65000,
-		VrfId:           1,
-		VrfName:         "default",
-		TableVersion:    9118504,
-		RibCount:        123456,
-		RibMemory:       48170280,
-		PeerCount:       10,
-		PeerMemory:      1234322,
-		PeerGroupCount:  3,
-		PeerGroupMemory: 192,
-		Peers: map[string]*pb.BgpIpv6SummaryPeerInfo{
-			"2001:db8:1000:1000::1": {
-				RemoteAs:                   64600,
-				LocalAs:                    64601,
-				Version:                    4,
-				MsgRcvd:                    211234,
-				MsgSent:                    9999,
-				TableVersion:               0,
-				Outq:                       0,
-				Inq:                        0,
-				PeerUptime:                 "3d10h20m",
-				PeerUptimeMsec:             123456,
-				PeerUptimeEstablishedEpoch: 123,
-				PfxRcd:                     123,
-				PfxSnt:                     321,
-				State:                      "Established",
-				PeerState:                  "OK",
-				ConnectionsEstablished:     5,
-				ConnectionsDropped:         2,
-				Desc:                       "lo0.hoge.fuga.vsix.wide.ad.jp",
-				IdType:                     "ipv6",
-			},
-			"2001:db8:1000:2000::1": {
-				RemoteAs:                   64800,
-				LocalAs:                    64801,
-				Version:                    4,
-				MsgRcvd:                    9823479,
-				MsgSent:                    734832,
-				TableVersion:               0,
-				Outq:                       0,
-				Inq:                        0,
-				PeerUptime:                 "6d11h40m",
-				PeerUptimeMsec:             123456,
-				PeerUptimeEstablishedEpoch: 1234,
-				PfxRcd:                     1232,
-				PfxSnt:                     3211,
-				State:                      "Established",
-				PeerState:                  "OK",
-				ConnectionsEstablished:     5,
-				ConnectionsDropped:         2,
-				Desc:                       "lo0.foo.bar.vsix.wide.ad.jp",
-				IdType:                     "ipv6",
-			},
-		},
-	}, nil
+	//TODO:  (cd /home/vsix/nfv-kit/ && sudo docker-compose exec frr bash -c "vtysh -c 'show bgp ipv6 sum json'") | jq ".ipv6Unicast" を exec
+	res, cmd_err := pipeline.Output(
+		[]string{"docker", "exec", "nfv-kit_frr_1", "bash", "-c", "vtysh -c 'show bgp ipv6 sum json'"},
+		[]string{"jq", ".ipv6Unicast"},
+	)
+	if cmd_err != nil {
+		fmt.Println(cmd_err)
+	}
+
+	var obj pb.ShowBgpIpv6SummaryResult
+	json_err := json.Unmarshal(res, &obj)
+	if json_err != nil {
+		fmt.Println(json_err)
+	}
+	return &obj, nil
 }
--- router/router.go
+++ router/router.go
@@ -1,100 +1,29 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"log"
+	"net"
 
 	pb "github.com/Sabaniki/frr_sdn_con/pb/api"
+	router "github.com/Sabaniki/frr_sdn_con/router/lib"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	res := `
-	{
-
-		  "routerId":"100.100.30.1",
-		  "as":65000,
-		  "vrfId":0,
-		  "vrfName":"default",
-		  "tableVersion":40,
-		  "ribCount":3,
-		  "ribMemory":552,
-		  "peerCount":2,
-		  "peerMemory":1480592,
-		  "peerGroupCount":1,
-		  "peerGroupMemory":64,
-		  "peers":{
-			"2001:200:e00:b0:4690:0:100:2":{
-			  "hostname":"bb02-blue.fujisawa.vsix.wide.ad.jp",
-			  "remoteAs":4690,
-			  "localAs":65000,
-			  "version":4,
-			  "msgRcvd":54831,
-			  "msgSent":54822,
-			  "tableVersion":0,
-			  "outq":0,
-			  "inq":0,
-			  "peerUptime":"04w1d21h",
-			  "peerUptimeMsec":2584036000,
-			  "peerUptimeEstablishedEpoch":1638307255,
-			  "pfxRcd":2,
-			  "pfxSnt":0,
-			  "state":"Established",
-			  "peerState":"OK",
-			  "connectionsEstablished":8,
-			  "connectionsDropped":7,
-			  "desc":"ve1602.bb02-blue.fujisawa.vsix.wide.ad.jp",
-			  "idType":"ipv6"
-			},
-			"2001:200:e00:b0:4690:0:100:3":{
-			  "hostname":"bb02-green.fujisawa.vsix.wide.ad.jp",
-			  "remoteAs":4690,
-			  "localAs":65000,
-			  "version":4,
-			  "msgRcvd":30939,
-			  "msgSent":30573,
-			  "tableVersion":0,
-			  "outq":0,
-			  "inq":0,
-			  "peerUptime":"01w4d13h",
-			  "peerUptimeMsec":997811000,
-			  "peerUptimeEstablishedEpoch":1639893480,
-			  "pfxRcd":2,
-			  "pfxSnt":0,
-			  "state":"Established",
-			  "peerState":"OK",
-			  "connectionsEstablished":412,
-			  "connectionsDropped":411,
-			  "desc":"ve1602.bb02-green.fujisawa.vsix.wide.ad.jp",
-			  "idType":"ipv6"
-			}
-		  },
-		  "failedPeers":0,
-		  "displayedPeers":2,
-		  "totalPeers":2,
-		  "dynamicPeers":0,
-		  "bestPath":{
-			"multiPathRelax":"false"
-		  }
-		}`
-	var obj pb.ShowBgpIpv6SummaryResult
-	err := json.Unmarshal([]byte(res), &obj)
+	//TODO:  (cd /home/vsix/nfv-kit/ && sudo docker-compose exec frr bash -c "vtysh -c 'show bgp ipv6 sum json'") | jq ".ipv6Unicast" を exec
+	port := 50051
+	listenport, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatalf("failed to listen: %v", err)
 	}
-	println(obj.Peers["2001:200:e00:b0:4690:0:100:3"].PeerState)
 
-	// port := 50051
-	// listenport, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	// if err != nil {
-	// 	log.Fatalf("failed to listen: %v", err)
-	// }
+	// gRPCサーバーの生成
+	server := grpc.NewServer()
+	rt := router.Router{}
+	pb.RegisterShowBgpIpv6SummaryServiceServer(server, &rt)
 
-	// // gRPCサーバーの生成
-	// server := grpc.NewServer()
-	// rt := router.Router{}
-	// pb.RegisterShowBgpIpv6SummaryServiceServer(server, &rt)
-
-	// reflection.Register(server)
-	// server.Serve(listenport)
+	reflection.Register(server)
+	server.Serve(listenport)
 }
